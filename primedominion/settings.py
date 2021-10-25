38bb8c4c863ee1b82db105e8159182d1acd19cd6
Gibson Dignity <gibsondignty@gmail.com>
2021-10-25 04:42:17 +0000

    Final changes

--- primedominion/settings.py
+++ primedominion/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS =  ['*']
 INSTALLED_APPS = [
     'app.apps.AppConfig',
     'accountant',
-    'computed_property',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
